96e8e38069cf80f33fa92e7f6a0e6ea2c466efaf
Monish Anand <137139542+MonishAnand7@users.noreply.github.com>
2023-11-19 12:30:27 +0530

    Update Caesar Cipher Text Encryption.py

--- Caesar Cipher Text Encryption.py
+++ Caesar Cipher Text Encryption.py
@@ -4,21 +4,22 @@ def Encrypt(text, shift):
     for char in text:
         if char.isalpha():
             is_upper = char.isupper()
-            char = char.lower()
-
-            encrypted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
-
-            if is_upper:
-                encrypted_char = encrypted_char.upper()
-
-            encrypted_text += encrypted_char
+            
+            shifted_char = chr((ord(char) - ord('A' if is_upper else 'a') + shift) % 26 + ord('A' if is_upper else 'a'))
+            encrypted_text += shifted_char
         else:
             encrypted_text += char
 
     return encrypted_text
 
-user_text = input("Enter the text to encrypt: ")
-user_shift = int(input("Enter the shift value: "))
+def Decrypt(text, shift):
+    return Encrypt(text, -shift)
 
-encrypted_result = Encrypt(user_text, user_shift)
-print(f"The encrypted text is: {encrypted_result}")
+user_text = input("Enter the text to encrypt: ")
+shift_amount = int(input("Enter the shift amount for encryption: "))
+
+encrypted_text = Encrypt(user_text, shift_amount)
+print(f"Encrypted text: {encrypted_text}")
+
+decrypted_text = Decrypt(encrypted_text, shift_amount)
+print(f"Decrypted text: {decrypted_text}")
